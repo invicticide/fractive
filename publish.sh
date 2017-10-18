8dff65fbe5ca85f1444c246f4b0af513fed15e93
Nathaniel Nelson <nelson.nleroy@gmail.com>
2017-10-17 22:51:07 -0600

    So much hacking later, you can choose not to bundle javascript files

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [[ $1 == "" ]] || [[ $2 == "" ]] ; then
-	echo Usage: ./publish.sh path/to/story/directory/ path/to/template.html
+if [[ $1 == "" ]] || [[ $2 == "" ]]  || [[ $3 == "" ]]; then
+	echo "Usage: ./publish.sh path/to/story/directory/ path/to/template.html [bundleScripts: true or false]"
 	exit 1
 fi
-node lib/Compiler.js $1 $2
+node lib/Compiler.js $1 $2 $3
 exit $?
