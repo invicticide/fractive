d18b9db286856478354141fac8cc8f569892bee9
Nathaniel Nelson <nelson.nleroy@gmail.com>
2017-11-06 16:20:21 -0700

    Refactored to give fractive-story-template an entry point for custom markdown loader

--- src/Compiler.ts
+++ src/Compiler.ts
@@ -263,6 +263,17 @@ export namespace Compiler
 		}
 		let text : string = fs.readFileSync(filepath, "utf8");
 
+    return RenderText(text);
+  }
+
+	/**
+	 * Renders the given Markdown text to HTML
+	 * @param text The complete text of the Markdown to render (can contain
+   * multiple sections)
+	 * @return The rendered HTML, or null on error
+	 */
+  export function RenderText(text: string) : string
+  {
 		// Set up parsing state
 		let braceCount : number = 0;
 		let sectionName : string = "";
