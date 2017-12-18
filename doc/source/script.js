beab944aee8c4e82ed726aa8e151598fd540da8d
Josh Sutphin <josh@kickbomb.us>
2017-12-17 17:03:32 -0700

    Fixed an issue where docs weren't resetting scroll position when changing topics

--- doc/source/script.js
+++ doc/source/script.js
@@ -8,6 +8,8 @@ Core.AddEventListener("OnBeginStory", () =>
 Core.AddEventListener("OnGotoSection", function(id, element, tags)
 {
 	// Scroll to top of new content
-	var contentDiv = document.getElementById("__content");
+	var contentDiv = document.getElementById("__contentContainer");
 	if(contentDiv) { contentDiv.scrollTop = 0; }
 });
+
+function InlineFunc() { return "Hello, world!"; }
