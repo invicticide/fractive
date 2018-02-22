9a07569e66424ad9cfd7d08cdef45d4789df197a
Nat Quayle Nelson <natquaylenelson@gmail.com>
2018-02-22 15:20:54 -0700

    Regex aliases can optionally be debugged

--- src/Compiler.ts
+++ src/Compiler.ts
@@ -1194,10 +1194,17 @@ export namespace Compiler
 							else if (alias.hasOwnProperty('regex'))
 							{
 								regexp = XRegExp(alias.regex);
-                                console.log(`Checking macro ${macroName} against regex ${alias.regex}`);
+                                if (alias.debug)
+                                {
+                                    console.log(`Checking macro ${macroName} against regex ${alias.regex}`);
+                                }
 								regexpToReplace = XRegExp('{' + (bIsEnd ? '/' : '') + alias.regex + '}');
 
 								if (regexp.exec(macroName)) {
+                                    if (alias.debug)
+                                    {
+                                        console.log(`Replacing macro ${macroName} with ${regexpReplacement}`);
+                                    }
 									regexpReplacement = (bIsEnd ? alias.end : alias.replaceWith);
                                     break;
 								}
@@ -1211,7 +1218,6 @@ export namespace Compiler
 						}
 						else if (regexpReplacement)
 						{
-                            console.log(`Replacing macro ${macroName} with ${regexpReplacement}`);
 							replacement = XRegExp.replace(regexpToReplace, regexpToReplace, regexpReplacement);
 							markdown = XRegExp.replace(markdown, regexpToReplace, regexpReplacement, 'all');
 							i += replacement.length - 1;
